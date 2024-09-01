db4313d247e7889f205998e3aff7d3d22b95b387
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-09-01 09:13:08 +0200

    history encoder

---
+++ src/encoders/history.py
@@ -0,0 +1,22 @@
+from typing import List
+
+import numpy as np
+
+from src.encoders.color import ColorPerspective
+from src.encoders.pieces import PiecesEncoder
+
+
+class HistoryEncoder:
+    def __init__(self, history_size: int):
+        self.history_size = history_size
+
+    def encode(self, past_fens: List[str], perspective: ColorPerspective) -> np.ndarray:
+        encoded_past_boards = []
+        for fen in past_fens:
+            encoded_past_boards += list(PiecesEncoder().encode(fen, perspective))
+        encoded_past_boards = np.array(encoded_past_boards)
+        if len(past_fens) < self.history_size:
+            missing_boards = self.history_size - len(past_fens)
+            empty_history = np.zeros((missing_boards * 12, 8, 8), dtype=int)
+            return np.concatenate((empty_history, encoded_past_boards), axis=0)
+        return encoded_past_boards
