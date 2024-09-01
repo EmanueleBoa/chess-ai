5af5709bf5dceb7a6717b1d45461bcec85b686d7
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-09-01 09:17:17 +0200

    convert static methode to instance method

--- src/encoders/pieces.py
+++ src/encoders/pieces.py
@@ -5,11 +5,10 @@ from src.processors import FenProcessor
 
 
 class PiecesEncoder:
-    @staticmethod
-    def encode(fen: str, perspective: ColorPerspective) -> np.ndarray:
+    def encode(self, fen: str, perspective: ColorPerspective) -> np.ndarray:
         board_string = FenProcessor().get_board_string(fen)
-        encoded_pieces = PiecesEncoder()._encode_pieces(board_string, perspective.pieces)
-        encoded_opponent_pieces = PiecesEncoder()._encode_pieces(board_string, perspective.opponent_pieces)
+        encoded_pieces = self._encode_pieces(board_string, perspective.pieces)
+        encoded_opponent_pieces = self._encode_pieces(board_string, perspective.opponent_pieces)
         return np.concatenate((encoded_pieces, encoded_opponent_pieces), axis=0)
 
     def _encode_pieces(self, board: str, pieces: str) -> np.ndarray:
