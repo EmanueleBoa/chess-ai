0e4562b49f1f0cfe5c7d62ea6e4fa66ecac32143
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-09-01 23:51:17 +0200

    minor

--- src/encoders/legal_moves.py
+++ src/encoders/legal_moves.py
@@ -4,7 +4,6 @@ import chess
 import numpy as np
 
 from src.encoders import BoardEncoder
-from src.encoders.color import ColorPerspective
 from src.encoders.history import HistoryEncoder
 from src.encoders.mate import MateEncoder
 from src.processors import FenProcessor
@@ -20,9 +19,9 @@ class LegalMovesEncoder:
         if self.history_encoder.is_history_needed():
             encoded_history = self.history_encoder.encode(history, color)
             encoded_starting_board = np.concatenate((encoded_history, encoded_starting_board), axis=0)
-        final_boards_fen = self._get_next_possible_board_fens(fen)
+        final_boards_fens = self._get_next_possible_boards_fens(fen)
         encoded_moves = []
-        for final_fen in final_boards_fen:
+        for final_fen in final_boards_fens:
             encoded_final_board = BoardEncoder().encode(final_fen, color=color)
             encoded_mate_terminations = MateEncoder().encode(final_fen)
             encoded_move = np.concatenate(
@@ -31,14 +30,14 @@ class LegalMovesEncoder:
         return np.array(encoded_moves)
 
     @staticmethod
-    def _get_next_possible_board_fens(starting_fen: str) -> List[str]:
+    def _get_next_possible_boards_fens(starting_fen: str) -> List[str]:
         starting_board = chess.Board(fen=starting_fen)
-        final_boards_fen = []
+        final_boards_fens = []
         for move in starting_board.legal_moves:
             final_board = starting_board.copy()
             final_board.push(move)
-            final_boards_fen.append(final_board.fen())
-        return final_boards_fen
+            final_boards_fens.append(final_board.fen())
+        return final_boards_fens
 
     @staticmethod
     def _get_legal_moves_uci(fen: str) -> List[str]:
